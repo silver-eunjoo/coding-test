728ca928792360db4771d0de9bd66afe03848528
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2024-04-03 16:35:10 +0900

    Solved

--- sorting/1.py
+++ sorting/1.py
@@ -1,15 +1,28 @@
 def solution(array, commands):
     answer = []
-    n = len(commands[0])
     
-    for i in range(n) :
-        i, j, k = commands[i][0], commands[i][1], commands[i][2]
+    for a,b,c in commands :
         
-        array2 = array[i-1:j]
+        array2 = array[a-1:b]
         array2.sort()
-        answer.append(array2[k-1])
+
+        answer.append(array2[c-1])
         
     return answer
+#이렇게 바꾸니 통과됐다!
+
+# def solution(array, commands):
+#     answer = []
+#     n = len(commands[0])
+    
+#     for i in range(n) :
+#         i, j, k = commands[i][0], commands[i][1], commands[i][2]
+        
+#         array2 = array[i-1:j]
+#         array2.sort()
+#         answer.append(array2[k-1])
+        
+#     return answer
 
 
 # def solution(array, commands):
