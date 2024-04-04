a1ad7481a1898b41f4829ec6fbe5816cc2038597
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2024-04-05 02:26:00 +0900

    solved

--- sorting/2.py
+++ sorting/2.py
@@ -1,22 +1,47 @@
-# 가장 큰 수
-def solution(numbers):
-    answer = ''
-    global temp
-    temp = []
-    for i in numbers :
-        if i < 10 :
-            temp.append(str(i))
-        else :
-            slicing(i)
-    
-    temp.sort(reverse=True)
-    answer = ''.join(temp)
-    return answer
+# final answer
+from functools import cmp_to_key
 
-def slicing(number) : 
-    while(number != 0) :
-        temp.append(str(number%10))
-        number = int(number/10)
+def compare(x,y) :
+    if x+y >= y+x : return -1
+    else : return 1
+
+def solution(numbers) :
+    answer = ''
+    numbers = list(map(str, numbers))
+    # print(numbers)
+    numbers.sort(key = cmp_to_key(compare))
+    # print(numbers)
+    answer = ''.join(numbers)
+    
+    if(answer[0] == '0') :
+        answer = '0'
+    
+    return answer
+# i solved this after getting hint tho
+# and i want to try another solution without using extra library
+
+
+
+# first try - failed
+# 가장 큰 수
+# def solution(numbers):
+#     answer = ''
+#     global temp
+#     temp = []
+#     for i in numbers :
+#         if i < 10 :
+#             temp.append(str(i))
+#         else :
+#             slicing(i)
+    
+#     temp.sort(reverse=True)
+#     answer = ''.join(temp)
+#     return answer
+
+# def slicing(number) : 
+#     while(number != 0) :
+#         temp.append(str(number%10))
+#         number = int(number/10)
 
 # 이렇게 했는데.. 테케가 실패하길래 다시 보니, 아마 1. 숫자 재배치 먼저 2. 그 후에 슬라이싱 없이 가장 큰 수를 만들어야 하는 것 같다.
 # 완전히 잘못 해석했구만.
