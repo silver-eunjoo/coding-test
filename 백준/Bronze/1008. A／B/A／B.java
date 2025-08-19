00155cb855fc20c940b51724ab3f7e6c7dd86b37
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-08-19 12:56:06 +0900

    [Bronze V] Title: A/B, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String line = br.readLine();
+		
+		StringTokenizer st = new StringTokenizer(line, " ");
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		bw.write(String.valueOf((double)a/b));
+        bw.flush();
+	}
+}
