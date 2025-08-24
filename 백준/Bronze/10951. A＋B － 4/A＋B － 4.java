f233d01da0f0ee6b12bcd1dc1fe4fcbfb224ba86
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-08-24 14:10:30 +0900

    [Bronze V] Title: A+B - 4, Time: 120 ms, Memory: 14692 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st;
+        String line;
+        
+        while((line = br.readLine()) != null) {
+            st = new StringTokenizer(line, " ");
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            bw.write(a + b + "\n");
+        }
+        bw.flush();
+    }
+}
