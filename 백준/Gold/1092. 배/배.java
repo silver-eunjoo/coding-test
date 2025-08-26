6493ddf391380ac6fe210755e343455334c776eb
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-08-26 22:13:47 +0900

    [Gold V] Title: 배, Time: 168 ms, Memory: 16484 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -2,45 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static List<Integer> cranes = new ArrayList<>();;
+    static List<Integer> boxes = new ArrayList<>();;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        Integer[] cranes = new Integer[N];
         st = new StringTokenizer(br.readLine(), " ");
         for(int i=0;i<N;i++){
-            cranes[i] = Integer.parseInt(st.nextToken());
+            cranes.add(Integer.parseInt(st.nextToken()));
         }
         int M = Integer.parseInt(br.readLine());
-        List<Integer> boxes = new ArrayList<>();
         st = new StringTokenizer(br.readLine(), " ");
         for(int i=0;i<M;i++){
             boxes.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(cranes, Collections.reverseOrder());
+        cranes.sort(Collections.reverseOrder());
         boxes.sort(Collections.reverseOrder());
-        
-        if(cranes[0] < boxes.get(0)) {
-            bw.write(String.valueOf(-1));
-            bw.flush();
-            return;
-        }
+        cranes.removeIf(x -> x < boxes.get(M-1));
+        bw.write(String.valueOf(findMinTime()));
+        bw.flush();
+    }
+    
+    static int findMinTime() {
+        if(cranes.isEmpty()) return -1;
+        if(cranes.get(0) < boxes.get(0)) return -1;
         int sec = 0;
-        int craneSize = cranes.length;
+        int craneSize = cranes.size();
         while(!boxes.isEmpty()) {
             int craneIdx = 0;  
             int boxIdx = 0;
             while(craneIdx < craneSize) {
                 if(boxIdx == boxes.size()) break;
-                if(cranes[craneIdx] >= boxes.get(boxIdx)) {
+                if(cranes.get(craneIdx) >= boxes.get(boxIdx)) {
                     boxes.remove(boxIdx);
                     craneIdx++;
                 } else boxIdx++;
             }
             sec++;
         }
-        bw.write(String.valueOf(sec));
-        bw.flush();
+        return sec;
     }
 }
