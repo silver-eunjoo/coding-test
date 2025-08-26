bf5c9689be1759e352cc42140f39d4801733a226
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-08-26 21:20:23 +0900

    [Gold V] Title: 배, Time: 4504 ms, Memory: 16620 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        Integer[] cranes = new Integer[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0;i<N;i++){
+            cranes[i] = Integer.parseInt(st.nextToken());
+        }
+        int M = Integer.parseInt(br.readLine());
+        List<Integer> boxes = new ArrayList<>();
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0;i<M;i++){
+            boxes.add(Integer.parseInt(st.nextToken()));
+        }
+        Arrays.sort(cranes, Collections.reverseOrder());
+        boxes.sort(Collections.reverseOrder());
+        
+        if(cranes[0] < boxes.get(0)) {
+            bw.write(String.valueOf(-1));
+            bw.flush();
+            return;
+        }
+        
+        int sec = 0;
+        while(!boxes.isEmpty()) {
+            for(int i=0;i<N;i++){
+                for(int j=0;j<boxes.size();j++){
+                    if(cranes[i] >= boxes.get(j)) {
+                        boxes.remove(j);
+                        break;
+                    }
+                }
+            }
+            sec++;
+        }
+        bw.write(String.valueOf(sec));
+        bw.flush();
+    }
+}
