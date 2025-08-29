368ea076e73ce86d9c958e4f0d35c7ed9c81ebf4
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-08-29 13:01:05 +0900

    [Silver I] Title: 팔, Time: 120 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.java
+++ 백준/Silver/1105. 팔/팔.java
@@ -2,25 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static String[] L;
-    static String[] R;
-    static int idx = -1;
+    static String L;
+    static String R;
 	public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-	    L = st.nextToken().split("");
-	    R = st.nextToken().split("");
+	    L = st.nextToken();
+	    R = st.nextToken();
 	    bw.write(countEight()+"\n");
 	    bw.flush();
 	}
 	static int countEight() {
-	    if(L.length!=R.length) return 0;
+	    if(L.length()!=R.length()) return 0;
 	    int total = 0;
-	    for(int i=0;i<L.length;i++){
-	        if(!L[i].equals(R[i])) break;
-	        if(L[i].equals("8")) total++;
-	    }
+	    for (int i = 0; i<L.length(); i++) {
+	        if (L.charAt(i) != R.charAt(i)) break;
+	        if (L.charAt(i) == '8') total++; 
+	    } 
 	    return total;
 	}
 }
