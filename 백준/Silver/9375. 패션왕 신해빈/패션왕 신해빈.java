c9705f6e29a32aa1be68815641bdce0ee85a19f1
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-09-08 16:26:15 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 132 ms, Memory: 15228 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        Map<String, Integer> closet;
+        while(T-->0){
+            closet = new HashMap<>();
+            int N = Integer.parseInt(br.readLine());
+            while(N-->0){
+                String[] input = br.readLine().split(" ");
+                int count = closet.getOrDefault(input[1], 0);
+                count++;
+                closet.put(input[1], count);
+            }
+            int result = 1;
+            for(String key : closet.keySet()) {
+                result *=closet.get(key)+1;
+            }
+            sb.append(result-1 + "\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+	}
+}
