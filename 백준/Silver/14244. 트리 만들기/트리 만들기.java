2c455ee8b376ca30eb33cb235c8e7557cf485784
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-09-17 13:09:48 +0200

    [Silver IV] Title: 트리 만들기, Time: 124 ms, Memory: 16068 KB -BaekjoonHub

--- 백준/Silver/14244. 트리 만들기/트리 만들기.java
+++ 백준/Silver/14244. 트리 만들기/트리 만들기.java
@@ -9,19 +9,13 @@ public class Main{
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken()); // 노드 수 
         int M = Integer.parseInt(st.nextToken()); // 리프 수
-        int[][] edges = new int[N][2];
         int i;
         for(i=0;i<N-M+1;i++){
-            edges[i][0] = i;
-            edges[i][1] = i+1;
+            sb.append(i + " " + (i+1) + "\n");
         }
         int left = M-1;
         while(left-->0) {
-            edges[i][0] = N-M;
-            edges[i][1] = ++i;
-        }
-        for(i=0;i<N-1;i++){
-            sb.append(edges[i][0] + " " + edges[i][1] + "\n");
+            sb.append(N-M + " " + ++i + "\n");
         }
         bw.write(sb.toString());
         bw.flush();
