9c61c5e6470238e1550a1c8484ffc248d9f1a6c3
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-09-25 10:00:04 +0200

    [Silver IV] Title: 숫자 카드 2, Time: 1092 ms, Memory: 191720 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        Map<Integer, Integer> map = new HashMap<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        while(N-->0) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        while(M-->0) {
+            int num = Integer.parseInt(st.nextToken());
+            if(map.containsKey(num)) sb.append(map.get(num) + " ");
+            else sb.append("0 ");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+	}
+}
