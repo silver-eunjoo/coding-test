ea9062a1b29ae70931ff44aace73d1fcda8ea50b
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-10-15 18:11:00 +0200

    [Bronze V] Title: 사랑은 고려대입니다, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		while(N-->0) {
+		    System.out.print("LoveisKoreaUniversity ");
+		}
+	}
+}
