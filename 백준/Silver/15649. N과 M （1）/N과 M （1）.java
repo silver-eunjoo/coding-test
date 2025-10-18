e7260ee553b28fa97323c8a989b533ba357d810d
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-10-18 12:11:09 +0200

    [Silver III] Title: N과 M (1), Time: 1756 ms, Memory: 75596 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static boolean[] visited;
+    static int[] selected;
+    public static void permutation(int depth) {
+        if(depth==M) {
+            for(int num : selected) System.out.print(num + " ");
+            System.out.println();
+            return;
+        }
+        
+        for(int i=1;i<=N;i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            selected[depth] = i;
+            permutation(depth+1);
+            visited[i] = false;
+        }
+    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visited = new boolean[N+1];
+		selected = new int[M];
+		permutation(0);
+	}
+}
