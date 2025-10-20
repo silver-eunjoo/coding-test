2b54fd0ed2c9bf499e33822d9f8da033ada570f1
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-10-20 14:57:23 +0200

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 392 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -2,15 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] A;
+    static int[] B;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		int[] A;
-		int[] B;
-		int left;
 		while(T-->0) {
 		    int answer = 0;
 		    st = new StringTokenizer(br.readLine());
@@ -26,18 +25,26 @@ public class Main {
 		    for(int i=0;i<M;i++){
 		        B[i] = Integer.parseInt(st.nextToken());
 		    }
-		    Arrays.sort(A);
 		    Arrays.sort(B);
-		    left = 0;
-            for(int i=0;i<N;i++){
-                while(left < M && A[i] > B[left]) {
-                    left++;
-                }
-                answer+=left;
-            }
+		    for(int a : A) {
+		        answer+=aBiggerThanB(a);
+		    }
             sb.append(answer+"\n");
 		}
 		bw.write(sb.toString());
 		bw.flush();
+	}
+	private static int aBiggerThanB(int target) {
+	    int left = 0;
+	    int right = B.length;
+	    while(left<right) {
+	        int mid = (left+right)/2;
+	        if(B[mid] < target) {
+	            left= mid+1;
+	        } else {
+	            right = mid;
+	        }
+	    }
+	    return left;
 	}
 }
