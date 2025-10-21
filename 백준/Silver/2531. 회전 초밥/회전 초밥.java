ae20498b92f1ed7a7e7e1589cbbf194e15b104af
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-10-21 19:17:42 +0900

    [Silver I] Title: 회전 초밥, Time: 184 ms, Memory: 19812 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -2,8 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] A;
-    static int[] B;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -27,14 +25,10 @@ public class Main {
 		for(int i=k;i<k+N-1;i++){
 		    int out = sushi[(i-k)%N];
 		    int in = sushi[i%N];
-		    if(count[out] == 1) { // 나갈 스시
-		        selected.remove(out);
-		        count[out]--;
-		    } else count[out]--;
-		    if(count[in]==0) { // 들어올 스시
-		        count[in]++;
-		        selected.add(in);
-		    } else count[in]++;
+		    if(count[out] == 1) selected.remove(out);
+		    count[out]--;
+		    if(count[in]==0) selected.add(in);
+		    count[in]++;
 		    max = Math.max(max, count[c]==0?selected.size()+1:selected.size());
 		}
 		bw.write(String.valueOf(max));
