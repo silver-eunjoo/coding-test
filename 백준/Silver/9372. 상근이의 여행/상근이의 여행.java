885f905162b49667ef40e182ec8ac3c0ac00d34b
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-10-28 00:23:27 +0900

    [Silver IV] Title: 상근이의 여행, Time: 240 ms, Memory: 32124 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -13,8 +13,6 @@ public class Main {
     		int M = Integer.parseInt(st.nextToken());
     		for(int i=0;i<M;i++){
     		    st = new StringTokenizer(br.readLine());
-    		    int a = Integer.parseInt(st.nextToken());
-    		    int b = Integer.parseInt(st.nextToken());
     		}
     		sb.append(N-1 + "\n");
 		}
