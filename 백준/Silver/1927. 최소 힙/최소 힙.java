8a09c7810a3cb351af92b2069dc9cbf25728ca71
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-11-01 15:35:32 +0900

    [Silver II] Title: 최소 힙, Time: 280 ms, Memory: 26908 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while(N-->0) {
+            int command = Integer.parseInt(br.readLine());
+            if(command == 0) {
+                if(pq.isEmpty()) sb.append("0").append("\n");
+                else sb.append(pq.poll()).append("\n");
+            } else {
+                pq.offer(command);
+            }
+        }
+		bw.write(sb.toString());
+		bw.flush();
+	}
+}
