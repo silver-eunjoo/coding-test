9cbddfbd5720db6c404fa6606cb94621d73281e7
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-11-02 00:44:51 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1048 ms, Memory: 126956 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -14,13 +14,14 @@ public class Main {
 		int[] snack = new int[N];
 		int total = 0;
 		st = new StringTokenizer(br.readLine());
+		int max = 0;
 		for(int i=0;i<N;i++){
 		    snack[i] = Integer.parseInt(st.nextToken());
+		    max = Math.max(max, snack[i]);
 		}
 		Arrays.sort(snack);
 		int maxLength = 0;
 	    int min = 1;
-		int max = snack[N-1];
 		while(min<=max) {
 		    int cnt=0;
 		    int mid = (min+max) / 2;
