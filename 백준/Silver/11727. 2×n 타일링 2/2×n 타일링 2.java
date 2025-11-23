99dda59b9f589dad4709071af7cc0b9d678398db
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-11-23 16:51:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 100 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[1001];
+		dp[1] = 1;
+		dp[2] = 3;
+		for(int i=3;i<=N;i++){
+		    dp[i] = (dp[i-1] + dp[i-2]*2)%10007;
+		}
+		bw.write(String.valueOf(dp[N]));
+		bw.flush();
+	}
+}
